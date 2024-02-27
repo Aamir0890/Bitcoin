47417212256f4225465bd779deacd5582db54c2a
Yourdad404 <arshadaamir09@gmail.com>
2024-02-27 22:53:36 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,11 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import axios from 'axios';
+import "./shim";
 
+import Transfer from './Wallet/Transfer';
+
+import { useEffect } from 'react';
+let bip39 = require('bip39')
+let bip32 = require('bip32')
+let bitcoin = require('bitcoinjs-lib')
+
+    let seed = bip39.mnemonicToSeedSync("mnemonic goes here")
+    let hdNode = bip32.fromSeed(seed)
+
+    let childNode = hdNode.deriveHardened(0)
+    let external = childNode.derive(1)
+    console.log(bitcoin.payments.p2pkh({pubkey: external.publicKey}))
+    
 export default function App() {
+   const press=async()=>{
+    let address='n3zTfZe28CP83fCs558JxqaR57qMz2STkg'
+    const response = await axios.get(`https://api.blockcypher.com/v1/btc/test3/addrs/${address}/balance`);
+    let data = response.data
+   
+    console.log(data.final_balance)  // Convert satoshis to BTC
+   }
+    
+    const fun=async()=>{
+      const  headers= {
+        'X-CMC_PRO_API_KEY': 'dd67e15a-1474-42b0-8db2-ffa3728eeb93'
+      }
+      const response = await axios.get('https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',headers)
+      console.log(response.data)
+    }
+    
+    useEffect(()=>{
+   fun()
+   press()
+    },[])
+  
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  
+
+     <Transfer/>
+  {/* <Test/> */}
     </View>
   );
 }
